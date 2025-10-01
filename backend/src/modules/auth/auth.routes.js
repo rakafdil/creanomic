cda3a2c9f819774cc84c5928e3ccf18d11f328b2
rakafdil <rakafadillah123@gmail.com>
2025-10-01 13:08:26 +0700

    fe files change to ts format and authentication almost complete(?)

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -1,14 +1,19 @@
-import express from "express";
-import { AuthController } from "./auth.controller.js";
+import express from "express"
+import { AuthController } from "./auth.controller.js"
 
 export default function authRouter(supabase) {
-    const router = express.Router();
-    const controller = new AuthController(supabase);
+    const router = express.Router()
+    const controller = new AuthController(supabase)
 
-    router.post("/signup", controller.signup);
-    router.post("/login", controller.login);
-    router.get("/profile", controller.getProfile);
-    router.post("/logout", controller.logout);
+    router.post("/signup", controller.signup)
+    router.post("/login", controller.login)
+    router.post("/confirm-email", controller.confirmEmail)
+    router.get("/profile", controller.getProfile)
+    router.post("/logout", controller.logout)
+    router.post("/forgot-password", controller.forgotPassword)
+    router.post("/forgot-password/reset", controller.handleResetPassword)
+    router.get("/callback", controller.callback)
+    router.get("/google-signin", controller.loginWithGoogle)
 
-    return router;
+    return router
 }
