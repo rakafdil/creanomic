79d6220e79874fbf0c9dada645c9c19d3b19fff7
rakafdil <rakafadillah123@gmail.com>
2025-10-04 06:49:24 +0700

    hero done

---
+++ frontend/src/app/components/Home/Hero.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image'
+import Button from '../Common/Button'
+
+const Hero = () => {
+    return (
+        <section className='relative w-full min-h-[1024px] flex items-end overflow-hidden pb-34'>
+            <div className='absolute inset-0 -z-10'>
+                <Image
+                    src='/assets/hero.svg'
+                    alt='Hero background'
+                    fill
+                    priority
+                    className='object-cover'
+                />
+                <div className='absolute inset-0 bg-[linear-gradient(90deg,rgba(0,0,0,0.4)_0%,rgba(102,102,102,0)_100%)]' />
+
+            </div>
+
+            <div className='flex flex-col gap-5 max-w-3xl pl-40 text-left items-start'>
+                <div id='medium3' className='backdrop-blur-xs px-7 py-3 rounded-[40px] bg-black/20'>Ekonomi Hijau & Pangan Inklusif</div>
+                <div className='flex flex-col gap-6'>
+                    <h1 id='big3' className='leading-tight font-semibold'>
+                        Pangan Segar Lokal<br />
+                        Tumbuh Bersama Komunitas.
+                    </h1>
+                    <p id='medium3' className='text-white/80'>
+                        GrowthWell adalah platform hibrida yang menghubungkan Anda langsung dengan hasil tani dan ternak terbaik dari petani lokal, sekaligus menyalurkan pangan bergizi untuk yang membutuhkan melalui program donasi.
+                    </p>
+                </div>
+                <Button className='self-start' iconSrc='assets/arrow.svg'>Get Started</Button>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
