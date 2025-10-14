006acae85eebacd0e791b9a45edd151245f98414
rakafdil <rakafadillah123@gmail.com>
2025-10-14 20:43:23 +0700

    fix lagi

--- frontend/src/app/components/Products/Cart/ConfirmButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function ConfirmButton() {
-    return (
-        <button className="mt-4 w-full bg-green-600 text-white py-2 rounded-xl hover:bg-green-700">
-            Confirm Payment
-        </button>
-    );
-}
